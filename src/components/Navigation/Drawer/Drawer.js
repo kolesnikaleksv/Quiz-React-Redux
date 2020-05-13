8d4f8f89aa302fb4d4ef00784019398aa29332bf
kolesnikaleks <kolesnikaleksv@gmail.com>
2020-05-13 16:12:44 +0300

    Beta version

---
+++ src/components/Navigation/Drawer/Drawer.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react'
+import classes from './Drawer.module.css'
+import {NavLink} from 'react-router-dom'
+import Backdrop from '../../UI/Backdrop/Backdrop'
+
+
+class Drawer extends Component {
+
+  clickHandler = () => {
+    this.props.onClose()
+  }
+
+  renderLinks(links) {
+    return links.map((link, index) => {
+      return (
+        <li key={index}>
+          <NavLink
+            to={link.to}
+            exact={link.exact}
+            activeClassName={classes.active}
+            onClick={this.clickHandler}
+          >
+            {link.label}
+          </NavLink>
+        </li>
+      )
+    })
+  }
+
+  render() {
+    const cls = [classes.Drawer]
+
+    if (!this.props.isOpen) {
+      cls.push(classes.close)
+    }
+
+    const links = [
+        {to: '/', label: 'List', exact: true}   
+    ]
+
+    console.log('Auth', this.props.isAuthenticated)
+
+    if (this.props.isAuthenticated) {
+      links.push({to: '/quiz-creator', label: 'Create test', exact: false})
+      links.push({to: '/logout', label: 'Quit', exact: false})
+    } else {
+      links.push({to: '/auth', label: 'Autorization', exact: false})
+    }
+
+    return (
+      <React.Fragment>
+        <nav className={cls.join(' ')}>
+          <ul>
+            { this.renderLinks(links) }
+          </ul>
+        </nav>
+        { this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null }
+      </React.Fragment>
+    )
+  }
+}
+
+export default Drawer
